f9ffbceb91b981ebfe9f3eac8999bd83c4efb000
Niels Jespersen <niels@jespersen.info>
2018-12-22 11:38:49 +0100

    Deploy script aded

--- az_docker_app/azure-cli-deploy-script.sh
+++ az_docker_app/azure-cli-deploy-script.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 az login
 az account set --subscription "Visual Studio Enterprise"
+az group create --name resGrpDockerApps --location "West Europe"
+az appservice plan create --name DockerizedAppServicePlan --resource-group resGrpDockerApps --sku B1 --is-linux
+az webapp create --resource-group resGrpDockerApps --plan DockerizedAppServicePlan --name njn-DockerizedMultiImage --multicontainer-config-type compose --multicontainer-config-file docker-compose.yml
+#az group delete --name resGrpDockerApps
