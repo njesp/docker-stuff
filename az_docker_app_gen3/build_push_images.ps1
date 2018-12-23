614c43a0936e97e1a2a03c71556f9dc05351979e
Niels Jespersen <niels@jespersen.info>
2018-12-23 11:03:41 +0100

    Cleanup

--- az_docker_app_gen3/build_push_images.ps1
+++ az_docker_app_gen3/build_push_images.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 # Byg og push images 
-docker login -u njesp -p CavinLondon
+docker login -u njesp 
 docker build -t njesp/azdemodb ./db
 docker build -t njesp/azdemowebapp ./app
 docker push njesp/azdemodb
