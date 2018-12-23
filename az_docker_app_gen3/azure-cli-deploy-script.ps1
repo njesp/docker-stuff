e68f4bc3115562f65dcd56293d0ba7e28c60dcb1
Niels Jespersen <niels@jespersen.info>
2018-12-23 19:54:40 +0100

    Variables extracted

--- az_docker_app_gen3/azure-cli-deploy-script.ps1
+++ az_docker_app_gen3/azure-cli-deploy-script.ps1
@@ -2,11 +2,16 @@
 # Deploy docker-compose multi container app til Azure App Services
 # Sletter eventuel eksisterende ressorcegruppe, bar-røv-princippet
 # Dedikeret ressourcegruppe gør at man bare kan slette den, så ryger alle ressourcer under med i købet
+$name = 'njn-DockerizedMultiImage'
+$resgrp = 'resGrpDockerApps'
+$srvplan = 'DockerizedAppServicePlan'
+$compose_file = 'docker-compose.yml'
 az login
 az account set --subscription "Visual Studio Enterprise"
-az group delete --name resGrpDockerApps --yes
-az group create --name resGrpDockerApps --location "West Europe"
-az appservice plan create --name DockerizedAppServicePlan --resource-group resGrpDockerApps --sku B1 --is-linux
-az webapp create --resource-group resGrpDockerApps --plan DockerizedAppServicePlan --name njn-DockerizedMultiImage --multicontainer-config-type compose --multicontainer-config-file docker-compose.yml
+az group delete --name $resgrp --yes
+az group create --name $resgrp --location "West Europe"
+az appservice plan create --name $srvplan --resource-group $resgrp --sku B1 --is-linux
+az webapp create --resource-group $resgrp --plan $srvplan --name $name --multicontainer-config-type compose --multicontainer-config-file $compose_file
+az webapp log config --resource-group  $resgrp --name $name --application-logging true --detailed-error-messages true --docker-container-logging filesystem --failed-request-tracing true --level verbose
 Write-Host 'Sig nej hvis der ikke skal ryddes op endnu'
-az group delete --name resGrpDockerApps
+az group delete --name $resgrp
