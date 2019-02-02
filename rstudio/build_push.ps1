273e85285c942f6665e732856946c2455b540fe5
Niels Jespersen <niels@jespersen.info>
2019-02-02 15:37:58 +0100

    Tried to add ROracle. Needs more work

--- rstudio/build_push.ps1
+++ rstudio/build_push.ps1
@@ -3,7 +3,8 @@
 # Der bygges oven på images vedligeholdt af https://www.rocker-project.org/
 # Der mangler en Oracle klient og ROracle pakken. Men lige nu er det Proof of Concept. 
 # Container sættes i gang med 
-# docker run --name rdst -d -p 8787:8787 -e PASSWORD=pw njesp/rdst
+# docker run --name rdst -d -p 8787:8787 -e PASSWORD=pw -v c:/Users/DstMove/Documents:/home/rstudio/Documents njesp/rdst 
+Write-Host 'Docker login'
 docker login -u njesp 
 docker build -t njesp/rdst .
 docker push njesp/rdst
---
+++ rstudio/todo.md
@@ -0,0 +1,8 @@
+# TODO
+Der mangler en Oracle klient og R-pakken ROracle. 
+Skulle kunne komme på via alien. Derefter bare ROracle pakken på sædvanlig vis. 
+```
+sudo apt-get install alien
+sudo alien -i oracle_client.rpm
+```
+
